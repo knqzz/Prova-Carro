418c55dad58a4956b7d4afbcfe8711693a82eebe
Kauan <mvkauan4@gmail.com>
2023-08-29 15:29:07 -0300

    Subi

--- src/controller/ClienteController.js
+++ src/controller/ClienteController.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { listarClientes} from "../repository/ClienteRepository.js";
+import { listarClientes, salvar, listarNomeCliente, apagar} from "../repository/ClienteRepository.js";
 
 let endpoints = Router();
 
@@ -7,6 +7,25 @@ endpoints.get('/listarclientes', async (req,resp) =>{
     let dados = await listarClientes();
     resp.send(dados);
 });
+
+endpoints.post('/salvar', async (req,resp) => {
+    let cliente = req.body;
+    let dados = await salvar(cliente);
+    resp.send(dados)
+});
+
+endpoints.get('/listarclientes/cliente', async(req,resp) => {
+    let nm = req.query.nome;
+    let dados = await listarNomeCliente(nm);
+    resp.send(dados)
+});
+
+endpoints.delete('/listarclientes/apagar', async(req,resp) =>{
+    let id = req.query.id;
+    let dados = await apagar(id)
+    resp.send('agenda apagada com sucesso!!')
+})
+
 
 
 export default endpoints
--- src/repository/ClienteRepository.js
+++ src/repository/ClienteRepository.js
@@ -9,7 +9,7 @@ export async function listarClientes() {
     return dados;
 }
 
-export async function inserir(cliente) {
+export async function salvar(cliente) {
     let sql = 'insert into tb_cliente (nm_cliente, ds_email, ds_telefone, ds_cpf, ds_cnh) values(?, ?, ?, ?, ?)'
     
     let resp = await conexao.query(sql, [cliente.nome, cliente.email, cliente.telefone, cliente.cpf, cliente.cnh]);
@@ -19,18 +19,18 @@ export async function inserir(cliente) {
     return cliente;
 }
 
-export async function listarNomeCliente() {
-    let sql = 'select nm_cliente as Cliente from tb_cliente where nm_cliente like ?'
+export async function listarNomeCliente(nm) {
+    let sql = 'select * from tb_cliente where nm_cliente like ?'
 
-    let resp = await conexao.query(sql)
+    let resp = await conexao.query(sql, ['%'+nm+'%'])
     let dados = resp[0]
 
     return dados;
 }
 
 export async function apagar(id){
-    let sql = 'delete from tb_cliente where id_cliente = ?';
-    let resp = await conexao.query(sql , [id]);
+    let sql = 'delete from tb_cliente where id_cliente like ?';
+    let resp = await conexao.query(sql , ['%'+id+'%']);
 
     let dados = resp[0];
     return dados;
